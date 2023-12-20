b37950e12c241917a2fe0e091828065b9bcebf02
alegreco7 <alexgreco2002@gmail.com>
2023-12-20 17:20:36 +0100

    exercise done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            emails : [ ]
+        }
+    },
+    created(){
+        for (let i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
+                this.emails.push(response.data.response);
+            });
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
